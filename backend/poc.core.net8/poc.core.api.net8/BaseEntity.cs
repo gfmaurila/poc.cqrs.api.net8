15f952337e2e7ccab9ee775098a4d77bccf7b9c3
Guilherme Figueiras Maurila <gfmaurila@gmail.com>
2024-07-21 17:53:58 -0300

    -

--- backend/poc.core.net8/poc.core.api.net8/BaseEntity.cs
+++ backend/poc.core.net8/poc.core.api.net8/BaseEntity.cs
@@ -13,7 +13,12 @@ public abstract class BaseEntity : IEntity<Guid>
 {
     private readonly List<Event> _domainEvents = new();
 
-    public Guid Id { get; private init; } = Guid.NewGuid();
+    public Guid Id { get; protected set; } = Guid.NewGuid();
+
+    public virtual void SetId(Guid id)
+    {
+        Id = id;
+    }
 
     /// <summary>
     /// Eventos de domínio que ocorreram.
