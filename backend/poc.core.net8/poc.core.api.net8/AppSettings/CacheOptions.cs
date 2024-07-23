4788c1aa28aa85b532878a118265a6f25b20b84e
Guilherme Figueiras Maurila <gfmaurila@gmail.com>
2024-07-23 12:17:11 -0300

    -

--- backend/poc.core.net8/poc.core.api.net8/AppSettings/CacheOptions.cs
+++ backend/poc.core.net8/poc.core.api.net8/AppSettings/CacheOptions.cs
@@ -3,12 +3,10 @@ using System.Diagnostics.CodeAnalysis;
 namespace poc.core.api.net8.AppSettings;
 
 [ExcludeFromCodeCoverage]
-public sealed class CacheOptions : BaseOptions
+public class CacheOptions
 {
     public const string ConfigSectionPath = nameof(CacheOptions);
-
-    public int AbsoluteExpirationInHours { get; private init; }
-    public int SlidingExpirationInSeconds { get; private init; }
-    public int DbIndex { get; private init; }
-
+    public int AbsoluteExpirationInHours { get; set; }
+    public int SlidingExpirationInSeconds { get; set; }
+    public int DbIndex { get; set; }
 }
